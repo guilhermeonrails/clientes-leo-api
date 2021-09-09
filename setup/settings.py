5b8a6f3e8c5a268766cb5840ccdb54b734e6ecc8
guilhermeonrails <30351153+guilhermeonrails@users.noreply.github.com>
2021-09-09 16:36:45 -0300

    add *

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$fu&5)8-mggsp+84o066b^qlz#ys%wg&%^$n3ot3dl#s*1v50$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
